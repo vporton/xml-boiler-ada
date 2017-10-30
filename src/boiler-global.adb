cf25b0e80f94ea773e3c3af0846575228f16da01
Victor Porton <porton@narod.ru>
2017-10-30 22:50:05 +0200

    bug fix

--- src/boiler-global.adb
+++ src/boiler-global.adb
@@ -55,8 +55,11 @@ package body Boiler.Global is
       Sub2: constant URI_Type := Copy(Sub);
       Super2: constant URI_Type := Copy(Super);
    begin
-      return Is_Ancestor(Node(Global.URIs_Map.Element(Sub2)),
-                         Node(Global.URIs_Map.Element(Super2)));
+      begin
+         return Is_Ancestor(Global.URIs_Map.Element(Sub2), Global.URIs_Map.Element(Super2));
+      exception
+         when Constraint_Error => return False;
+      end;
    end;
 
 end Boiler.Global;
