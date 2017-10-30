edb64a7916e4879cfa960e4e4e5ac89ca427e8df
Victor Porton <porton@narod.ru>
2017-10-30 20:09:25 +0200

    started to work on initializing directories

--- src/boiler-directories.ads
+++ src/boiler-directories.ads
@@ -1,7 +1,21 @@
+private with Ada.Finalization;
+private with Ada.Strings.Unbounded;
+
 package Boiler.Directories is
 
    -- Influenced by XMLBOILER_PATH environment variable
-   
-  function Get_Data_Dir return String; 
+   type Directories_Config_Type is limited private;
+
+   -- Get "data" dir (usually share/)
+   function Get_Data_Dir (Config: Directories_Config_Type) return String;
+
+private
+
+   type Directories_Config_Type is new Ada.Finalization.Limited_Controlled with
+      record
+         Root: Ada.Strings.Unbounded.Unbounded_String;
+      end record;
+
+   overriding procedure Initialize (Object: in out Directories_Config_Type);
 
 end Boiler.Directories;
--- src/boiler-global.ads
+++ src/boiler-global.ads
@@ -1,3 +1,4 @@
+with Boiler.Directories;
 with RDF.Redland.URI; use RDF.Redland.URI;
 private with Generic_Directed_Graph;
 
@@ -16,6 +17,7 @@ private
 
    type Global_State_Type is
       record
+         Directories_Config: Boiler.Directories.Directories_Config_Type;
          Sub_Classes_Graph: Sub_Classes_Graph_Type;
       end record;
 
