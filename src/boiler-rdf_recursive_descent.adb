8e1c472501000734dee7f246419a032d1a037878
Victor Porton <porton@narod.ru>
2017-11-08 00:31:53 +0200

    rewritten with indefinite types using function return values

--- src/boiler-rdf_recursive_descent.adb
+++ src/boiler-rdf_recursive_descent.adb
@@ -4,40 +4,30 @@ package body Boiler.RDF_Recursive_Descent is
 
    package body Zero_One_Predicate is
 
-      procedure Parse(World: Redland_World_Type_Without_Finalize'Class;
-                      Parser: Zero_One_Predicate_Parser;
-                      Model: Model_Type_Without_Finalize'Class;
-                      Node: Node_Type_Without_Finalize'Class;
-                      Data: Predicate_Parser.Data_Access) is
+      function Parse(World: Redland_World_Type_Without_Finalize'Class;
+                     Parser: Zero_One_Predicate_Parser;
+                     Model: Model_Type_Without_Finalize'Class;
+                     Node: Node_Type_Without_Finalize'Class)
+                     return Holder_Type is
          Iterator: Node_Iterator_Type :=
            Get_Targets(Model, Node, From_URI(World, Parser.Predicate));
          Child_Nodes: constant Node_Array := To_Array(Iterator);
          use Predicate_Parser;
       begin
          if Child_Nodes'Length /= 1 then
-            if Data /= null then
-               Data.all := Default_Value;
-            end if;
-            return;
+            return Default_Value;
          end if;
          declare
             Child_Node: Node_Type renames Child_Nodes(Child_Nodes'First);
-            Child: aliased Child_Type;
             use Node_Parser;
          begin
-            Parse(World,
-                  Parser.Child_Parser.all,
-                  Model,
-                  Child_Node,
-                  (if Data /= null then Child'Unchecked_Access else null));
-            if Data /= null then
-               Data.all := To_Holder(Child);
-            end if;
+            return To_Holder(Parse(World,
+                             Parser.Child_Parser.all,
+                             Model,
+                             Child_Node));
          exception
             when Parse_Error =>
-               if Data /= null then
-                  Data.all := Default_Value;
-               end if;
+               return Default_Value;
          end;
       end;
 
@@ -45,37 +35,29 @@ package body Boiler.RDF_Recursive_Descent is
 
    package body Zero_Or_More_Predicate is
 
-      procedure Parse(World: Redland_World_Type_Without_Finalize'Class;
-                      Parser: Zero_Or_More_Predicate_Parser;
-                      Model: Model_Type_Without_Finalize'Class;
-                      Node: Node_Type_Without_Finalize'Class;
-                      Data: Predicate_Parser.Data_Access) is
+      function Parse(World: Redland_World_Type_Without_Finalize'Class;
+                     Parser: Zero_Or_More_Predicate_Parser;
+                     Model: Model_Type_Without_Finalize'Class;
+                     Node: Node_Type_Without_Finalize'Class)
+                     return Vectors.Vector is
          Iter: Node_Iterator_Type :=
            Get_Targets (Model, Node, From_URI(World, Parser.Predicate));
-         use type Predicate_Parser.Data_Access;
          use Vectors, Node_Parser;
       begin
-         if Data /= null then
-            Clear(Data.all);
-         end if;
-         while not Is_End(Iter) loop
-            declare
-               Elt: aliased Temporary_Child_Type;
-            begin
-               Parse(World,
-                     Parser.Child_Parser.all,
-                     Model,
-                     Get_Node(Iter),
-                     (if Data /= null then Elt'Unchecked_Access else null));
-               if Data /= null then
-                  Append(Data.all, Child_Converter(Elt));
-               end if;
-            exception
-               when Parse_Error =>
-                  null; -- ignore non-parsed childs
-            end;
-            Next(Iter);
-         end loop;
+         return Result: Vectors.Vector do
+            while not Is_End(Iter) loop
+               begin
+                  Append(Result, Child_Converter(Parse(World,
+                         Parser.Child_Parser.all,
+                         Model,
+                         Get_Node(Iter))));
+               exception
+                  when Parse_Error =>
+                     null; -- ignore non-parsed childs
+               end;
+               Next(Iter);
+            end loop;
+         end return;
       end;
 
    end Zero_Or_More_Predicate;
--- src/boiler-rdf_recursive_descent.ads
+++ src/boiler-rdf_recursive_descent.ads
@@ -10,45 +10,42 @@ package Boiler.RDF_Recursive_Descent is
    Parse_Error: exception;
 
    generic
-      type Data_Type is private; -- for indefinite types use indefinite holder
+      type Data_Type (<>) is private;
    package Base_Predicate is
 
-      type Data_Access is access all Data_Type;
-
-      type Base_Predicate_Parser is tagged
+      type Base_Predicate_Parser is abstract tagged
          record
             Predicate: URI_Type;
---              Data: Data_Access; -- also make a version where it is a part of a larger record?
          end record;
 
       -- TODO: condition to restrict Node to only URI or Blank
-      not overriding procedure Parse (World: Redland_World_Type_Without_Finalize'Class;
-                                      Parser: Base_Predicate_Parser;
-                                      Model: Model_Type_Without_Finalize'Class;
-                                      Node: Node_Type_Without_Finalize'Class;
-                                      Data: Data_Access) is null;
+      not overriding function Parse (World: Redland_World_Type_Without_Finalize'Class;
+                                     Parser: Base_Predicate_Parser;
+                                     Model: Model_Type_Without_Finalize'Class;
+                                     Node: Node_Type_Without_Finalize'Class)
+                                     return Data_Type
+                                     is abstract;
 
    end Base_Predicate;
 
    generic
-      type Data_Type is private; -- for indefinite types use indefinite holder
+      type Data_Type (<>) is private; -- for indefinite types use indefinite holder
    package Base_Node is
 
-      type Data_Access is access all Data_Type;
+      type Base_Node_Parser is abstract tagged limited null record;
 
-      type Base_Node_Parser is tagged limited null record;
-
-      not overriding procedure Parse (World: Redland_World_Type_Without_Finalize'Class;
-                                      Parser: Base_Node_Parser;
-                                      Model: Model_Type_Without_Finalize'Class;
-                                      Node: Node_Type_Without_Finalize'Class;
-                                      Data: Data_Access) is null;
+      not overriding function Parse (World: Redland_World_Type_Without_Finalize'Class;
+                                     Parser: Base_Node_Parser;
+                                     Model: Model_Type_Without_Finalize'Class;
+                                     Node: Node_Type_Without_Finalize'Class)
+                                     return Data_Type
+                                     is abstract;
 
    end Base_Node;
 
    generic
-      type Child_Type is private;
-      type Holder_Type is private;
+      type Child_Type (<>) is private;
+      type Holder_Type (<>) is private;
       with function To_Holder (From: Child_Type) return Holder_Type;
       Default_Value: Holder_Type;
    package Zero_One_Predicate is
@@ -58,15 +55,15 @@ package Boiler.RDF_Recursive_Descent is
          record
             Child_Parser: access Node_Parser.Base_Node_Parser'Class;
          end record;
-      overriding procedure Parse(World: Redland_World_Type_Without_Finalize'Class;
-                                 Parser: Zero_One_Predicate_Parser;
-                                 Model: Model_Type_Without_Finalize'Class;
-                                 Node: Node_Type_Without_Finalize'Class;
-                                 Data: Predicate_Parser.Data_Access);
+      overriding function Parse(World: Redland_World_Type_Without_Finalize'Class;
+                                Parser: Zero_One_Predicate_Parser;
+                                Model: Model_Type_Without_Finalize'Class;
+                                Node: Node_Type_Without_Finalize'Class)
+                                return Holder_Type;
    end Zero_One_Predicate;
 
    generic
-      type Child_Type is private;
+      type Child_Type (<>) is private;
    package Simple_Zero_One_Predicate is
       package Holders is new Ada.Containers.Indefinite_Holders(Child_Type);
       package Parent is new Zero_One_Predicate(Child_Type,
@@ -78,7 +75,7 @@ package Boiler.RDF_Recursive_Descent is
 
    generic
       type Child_Type (<>) is private;
-      type Temporary_Child_Type is private;
+      type Temporary_Child_Type (<>) is private;
       with function Child_Converter (From: Temporary_Child_Type) return Child_Type;
    package Zero_Or_More_Predicate is
       package Vectors is new Ada.Containers.Indefinite_Vectors(Natural, Child_Type); -- It could work faster without "Indefinite"
@@ -88,15 +85,15 @@ package Boiler.RDF_Recursive_Descent is
          record
             Child_Parser: access Node_Parser.Base_Node_Parser'Class;
          end record;
-      overriding procedure Parse(World: Redland_World_Type_Without_Finalize'Class;
-                                 Parser: Zero_Or_More_Predicate_Parser;
-                                 Model: Model_Type_Without_Finalize'Class;
-                                 Node: Node_Type_Without_Finalize'Class;
-                                 Data: Predicate_Parser.Data_Access);
+      overriding function Parse(World: Redland_World_Type_Without_Finalize'Class;
+                                Parser: Zero_Or_More_Predicate_Parser;
+                                Model: Model_Type_Without_Finalize'Class;
+                                Node: Node_Type_Without_Finalize'Class)
+                                return Vectors.Vector;
    end Zero_Or_More_Predicate;
 
    generic
-      type Child_Type is private;
+      type Child_Type (<>) is private;
    package Simple_Zero_Or_More_Predicate is
       function Identity (From: Child_Type) return Child_Type is (From);
       package Parent is new Zero_Or_More_Predicate(Child_Type, Child_Type, Identity);
