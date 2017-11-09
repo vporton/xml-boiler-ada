e800ce9b484809cf2deff52ebc0616e02458b6b5
Victor Porton <porton@narod.ru>
2017-11-09 22:21:41 +0200

    Integer nodes

--- src/boiler-rdf_recursive_descent-literals.adb
+++ src/boiler-rdf_recursive_descent-literals.adb
@@ -12,4 +12,21 @@ package body Boiler.RDF_Recursive_Descent.Literals is
       return As_String(Node);
    end;
 
+   function Parse (World: Redland_World_Type_Without_Finalize'Class;
+                   Parser: Integer_Literal_Parser;
+                   Model: Model_Type_Without_Finalize'Class;
+                   Node: Node_Type_Without_Finalize'Class)
+                   return Integer is
+   begin
+      if not Is_Literal(Node) or else As_String(Get_Datatype_URI(Node)) /= "http://www.w3.org/2001/XMLSchema#integer" then
+         raise Parse_Error;
+      end if;
+      begin
+         return Integer'Value(As_String(Node));
+      exception
+         when Constraint_Error =>
+            raise Parse_Error;
+      end;
+   end;
+
 end Boiler.RDF_Recursive_Descent.Literals;
--- src/boiler-rdf_recursive_descent-literals.ads
+++ src/boiler-rdf_recursive_descent-literals.ads
@@ -4,10 +4,20 @@ package Boiler.RDF_Recursive_Descent.Literals is
 
    type String_Literal_Parser is new String_Node.Base_Node_Parser with null record;
 
-   function Parse (World: Redland_World_Type_Without_Finalize'Class;
-                   Parser: String_Literal_Parser;
-                   Model: Model_Type_Without_Finalize'Class;
-                   Node: Node_Type_Without_Finalize'Class)
-                   return String;
+   overriding function Parse (World: Redland_World_Type_Without_Finalize'Class;
+                              Parser: String_Literal_Parser;
+                              Model: Model_Type_Without_Finalize'Class;
+                              Node: Node_Type_Without_Finalize'Class)
+                              return String;
+
+   package Integer_Node is new Base_Node(Integer);
+
+   type Integer_Literal_Parser is new Integer_Node.Base_Node_Parser with null record;
+
+   overriding function Parse (World: Redland_World_Type_Without_Finalize'Class;
+                              Parser: Integer_Literal_Parser;
+                              Model: Model_Type_Without_Finalize'Class;
+                              Node: Node_Type_Without_Finalize'Class)
+                              return Integer;
 
 end Boiler.RDF_Recursive_Descent.Literals;
