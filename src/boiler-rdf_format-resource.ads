28e4239b99ddcec8d127491501030fa664669b61
Victor Porton <porton@narod.ru>
2017-11-13 04:48:21 +0200

    started work on :script RDF

--- src/boiler-rdf_format-resource.ads
+++ src/boiler-rdf_format-resource.ads
@@ -1,1 +1,11 @@
 package Boiler.RDF_Format.Resource is
+
+   type Transformer_Kind_Enum is (Entire, Sequential, Up_Down, Down_Up);
+
+   type Script_Info is abstract
+      record
+         Transforms_Kind: Transformer_Kind_Enum; -- unused when validation
+         Completeness, Stability, Preference: Long_Float; -- or better just Float?
+      end record;
+
+end Boiler.RDF_Format.Resource;
