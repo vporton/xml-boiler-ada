c0b16b874be4709aa30eeef7e6521ded80bd6b63
Victor Porton <porton@narod.ru>
2017-11-22 00:25:13 +0200

    locale categories

--- src/bindings/glibc-locale.ads
+++ src/bindings/glibc-locale.ads
@@ -9,6 +9,35 @@ package Glibc.Locale is
 
    Empty_Locale: constant Locale_Type;
 
+   type Locale_Category is mod 2**16;
+
+   LC_CTYPE_MASK          : constant Locale_Category := 2 ** LC_CTYPE;
+   LC_NUMERIC_MASK        : constant Locale_Category := 2 ** LC_NUMERIC;
+   LC_TIME_MASK           : constant Locale_Category := 2 ** LC_TIME;
+   LC_COLLATE_MASK        : constant Locale_Category := 2 ** LC_COLLATE;
+   LC_MONETARY_MASK       : constant Locale_Category := 2 ** LC_MONETARY;
+   LC_MESSAGES_MASK       : constant Locale_Category := 2 ** LC_MESSAGES;
+   LC_PAPER_MASK          : constant Locale_Category := 2 ** LC_PAPER;
+   LC_NAME_MASK           : constant Locale_Category := 2 ** LC_NAME;
+   LC_ADDRESS_MASK        : constant Locale_Category := 2 ** LC_ADDRESS;
+   LC_TELEPHONE_MASK      : constant Locale_Category := 2 ** LC_TELEPHONE;
+   LC_MEASUREMENT_MASK    : constant Locale_Category := 2 ** LC_MEASUREMENT;
+   LC_IDENTIFICATION_MASK : constant Locale_Category := 2 ** LC_IDENTIFICATION;
+   LC_ALL_MASK            : constant Locale_Category :=
+     LC_CTYPE_MASK or
+     LC_NUMERIC_MASK or
+     LC_TIME_MASK or
+     LC_COLLATE_MASK or
+     LC_MONETARY_MASK or
+     LC_MESSAGES_MASK or
+     LC_PAPER_MASK or
+     LC_NAME_MASK or
+     LC_ADDRESS_MASK or
+     LC_TELEPHONE_MASK or
+     LC_MEASUREMENT_MASK or
+     LC_IDENTIFICATION_MASK;
+
+
    function New_Locale (Category_Mask: int; Locale: String) return Locale_Type;
 
    function Use_Locale (Locale: Locale_Type) return Locale_Type;
--- src/bindings/glibc.ads
+++ src/bindings/glibc.ads
@@ -1,14 +1,19 @@
 package Glibc is
 
    --  Category values
-   LC_ALL      : constant Integer := 0;
-   LC_COLLATE  : constant Integer := 1;
-   LC_CTYPE    : constant Integer := 2;
-   LC_MONETARY : constant Integer := 3;
-   LC_NUMERIC  : constant Integer := 4;
-   LC_TIME     : constant Integer := 5;
-   LC_MESSAGES : constant Integer := 6;
-   u_LC_LAST   : constant Integer := 7;  --  Marks end
+   LC_CTYPE          : constant := 0;
+   LC_NUMERIC        : constant := 1;
+   LC_TIME           : constant := 2;
+   LC_COLLATE        : constant := 3;
+   LC_MONETARY       : constant := 4;
+   LC_MESSAGES       : constant := 5;
+   LC_ALL            : constant := 6;
+   LC_PAPER          : constant := 7;
+   LC_NAME           : constant := 8;
+   LC_ADDRESS        : constant := 9;
+   LC_TELEPHONE      : constant := 10;
+   LC_MEASUREMENT    : constant := 11;
+   LC_IDENTIFICATION : constant := 12;
 
    procedure Setlocale (Category : Integer := LC_ALL; Locale : String := "");
 
