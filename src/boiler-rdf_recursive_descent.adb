0a4d87736f7e4d425a264c5a940c9cb9f472e3b7
Victor Porton <porton@narod.ru>
2017-11-22 20:53:13 +0200

    license notices in source files

--- src/boiler-rdf_recursive_descent.adb
+++ src/boiler-rdf_recursive_descent.adb
@@ -1,3 +1,20 @@
+--  Copyright 2017 Victor Porton
+--
+--  This file is part of XML Boiler.
+--
+--  XML Boiler is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  XML Boiler is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with XML Boiler.  If not, see <http://www.gnu.org/licenses/>.
+
 with RDF.Redland.Statement; use RDF.Redland.Statement;
 with RDF.Redland.Node_Iterator; use RDF.Redland.Node_Iterator;
 with RDF.Redland.Stream; use RDF.Redland.Stream;
