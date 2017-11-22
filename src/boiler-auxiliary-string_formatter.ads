93f3c2dbed27011f0c42542db2ccd069eef75885
Victor Porton <porton@narod.ru>
2017-11-22 20:35:45 +0200

    started work on string formatter

--- src/boiler-auxiliary-string_formatter.ads
+++ src/boiler-auxiliary-string_formatter.ads
@@ -1,5 +1,15 @@
+-- This file is released not only under GPLv3 but also under Apache License 2.0
+
+-- Template strings like: "First phrase: {1}. Second phrase: {2}."
+-- If you need { before a natural number, then double it: {{.
+-- Double {{ and }} are interpreted as single { or }.
+
+--  with Ada.Strings.Unbounded;
+
 package Boiler.Auxiliary.String_Formatter is
 
---    function Format_String
+--     type Arguments_Type is array (Natural range <>) of Ada.Strings.Unbounded.Unbounded_String;
+--
+--     function Format_String (Format_String: String; Args: Arguments_Type) return String;
 
 end Boiler.Auxiliary.String_Formatter;
